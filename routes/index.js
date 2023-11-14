73a0c0b4e3470bfd6c9d8cc7a1e8a279c1b72f7b
nhkhanh21 <nhkhanh21@clc.fitus.edu.vn>
2023-11-14 20:27:28 +0700

    complete

--- routes/index.js
+++ routes/index.js
@@ -18,4 +18,28 @@ router.get('/contact', function (req, res, next) {
   res.render('user/contact', { title: 'Thông tin liên hệ', layout: 'user_layout.hbs' });
 });
 
+router.get('/about-us', function (req, res, next) {
+  res.render('user/about_us', { title: 'Thông tin liên hệ', layout: 'user_layout.hbs' });
+});
+
+router.get('/admin/', function (req, res, next) {
+  res.render('admin/admin', { title: 'Thông tin liên hệ', layout: 'admin_layout.hbs' });
+});
+
+router.get('/admin/app-chat', function (req, res, next) {
+  res.render('admin/app_chat', { title: 'Thông tin liên hệ', layout: 'admin_layout.hbs' });
+});
+
+router.get('/admin/app-calendar', function (req, res, next) {
+  res.render('admin/app_calendar', { title: 'Thông tin liên hệ', layout: 'admin_layout.hbs' });
+});
+
+router.get('/admin/ticket-list', function (req, res, next) {
+  res.render('admin/ticket_list', { title: 'Thông tin liên hệ', layout: 'admin_layout.hbs' });
+});
+
+router.get('/admin/about-us', function (req, res, next) {
+  res.render('admin/about_us', { title: 'Thông tin liên hệ', layout: 'admin_layout.hbs' });
+});
+
 module.exports = router;
