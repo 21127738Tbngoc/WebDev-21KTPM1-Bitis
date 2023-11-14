7c3e3fb7d96182dcf6e99ad6513302b0ba00417f
nhkhanh21 <nhkhanh21@clc.fitus.edu.vn>
2023-11-14 14:57:27 +0700

    user_layout.hbs

--- routes/index.js
+++ routes/index.js
@@ -2,20 +2,20 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Biti\'s' });
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Biti\'s', layout: 'user_layout.hbs' });
 });
 
-router.get('/shop-nam', function(req, res, next) {
-  res.render('user/shop_nam', { title: 'Biti\'s' });
+router.get('/shop-nam', function (req, res, next) {
+  res.render('user/shop_nam', { title: 'Biti\'s', layout: 'user_layout.hbs' });
 });
 
-router.get('/shop-nu', function(req, res, next) {
-  res.render('user/shop_nu', { title: 'Biti\'s' });
+router.get('/shop-nu', function (req, res, next) {
+  res.render('user/shop_nu', { title: 'Biti\'s', layout: 'user_layout.hbs' });
 });
 
-// router.get('/contact', function(req, res, next) {
-//   res.render('user/contact', { title: 'Biti\'s' });
-// });
+router.get('/contact', function (req, res, next) {
+  res.render('user/contact', { title: 'Biti\'s', layout: 'user_layout.hbs' });
+});
 
 module.exports = router;
--- routes/shop_nu.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/shop-nu', function(req, res, next) {
-  res.render('user/shop_nu', {layout: 'layout.hbs'});
-});
-
-module.exports = router;
