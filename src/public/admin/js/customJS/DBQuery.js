a0c53b2c6c7a713d322b6fc6b353ff945181a356
Ngoc Tran <tbngoc21@clc.fitus.edu.vn>
2024-01-07 14:28:00 +0700

    delete colors option in product detail

--- src/public/admin/js/customJS/DBQuery.js
+++
@@ -1,8 +0,0 @@
-const axios = require("axios");
-
-async function ProductQuery(filter)
-{
-    let response = await axios.get('http://localhost:3000/product/', {params: filter});
-    response = response.data;
-    return response;
-}
